6d858837f924b6bccd2e8e682d6418bbe3cebce2
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-13 22:32:25 -0300

    social media dashboard

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Social from './lib/Social.svelte';
+</script>
+
+<main>
+  <Social />
+</main>
+
+<style>
+  :global(body.dark-mode) {
+    background-color: var(--White-BG);
+    color: #bfc2c7;
+  }
+</style>
---
+++ src/lib/Social.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Header from './Header.svelte';
+</script>
+
+<main>
+  <div class="container">
+    <Header />
+  </div>
+</main>
+
+<style>
+  .container {
+    border: 1px solid white;
+    margin: 3rem auto;
+    max-width: 144rem;
+  }
+</style>
